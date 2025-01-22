e29a7f21405619386048cedaf0b3d791349e2e3f
julydeath <manojkarajada.mk@gmail.com>
2025-01-22 15:53:01 +0530

    init : drizzle and postgresql compose file

---
+++ server/adapter.ts
@@ -0,0 +1,13 @@
+// Make sure to install the 'postgres' package
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  DATABASE_URL: z.string().url(),
+});
+
+const processEnv = EnvSchema.parse(process.env);
+
+const queryClient = postgres(processEnv.DATABASE_URL);
+const db = drizzle({ client: queryClient });
