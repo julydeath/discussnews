60af7406f1babcb45753500f687665e99d4ddb15
julydeath <manojkarajada.mk@gmail.com>
2025-01-24 19:10:15 +0530

    init : frontend

--- server/adapter.ts
+++ server/adapter.ts
@@ -2,6 +2,16 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { z } from "zod";
+import { sessionTable, userRelations, userTable } from "./db/schemas/auth";
+import { postsRelations, postsTable } from "./db/schemas/posts";
+import { commentRelations, commentsTable } from "./db/schemas/comments";
+import {
+  commentUpvoteRelations,
+  commentUpvotesTable,
+  postUpvoteRelations,
+  postUpvotesTable,
+} from "./db/schemas/upvotes";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 
 const EnvSchema = z.object({
   DATABASE_URL: z.string().url(),
@@ -10,4 +20,24 @@ const EnvSchema = z.object({
 const processEnv = EnvSchema.parse(process.env);
 
 const queryClient = postgres(processEnv.DATABASE_URL);
-export const db = drizzle({ client: queryClient });
+export const db = drizzle(queryClient, {
+  schema: {
+    user: userTable,
+    session: sessionTable,
+    posts: postsTable,
+    comments: commentsTable,
+    postUpvotes: postUpvotesTable,
+    commentUpvoted: commentUpvotesTable,
+    postsRelations,
+    commentUpvoteRelations,
+    postUpvoteRelations,
+    userRelations,
+    commentRelations,
+  },
+});
+
+export const adapter = new DrizzlePostgreSQLAdapter(
+  db,
+  sessionTable,
+  userTable,
+);
