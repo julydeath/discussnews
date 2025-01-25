b54b91fad3461f7dc4fde6c42543fba3044c68aa
julydeath <manojkarajada.mk@gmail.com>
2025-01-25 15:13:07 +0530

    chore: sign up and login pages

--- frontend/src/components/Navbar/Navnar.tsx
+++ frontend/src/components/Navbar/Navnar.tsx
@@ -46,36 +46,49 @@ export const Navbar = () => {
         >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 md:items-center">
             <li>
-              <a
-                href="#"
+              <Link
+                to="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
               >
-                <Link to="/">New</Link>
-              </a>
+                New
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                to="/"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                <Link to="/">Top</Link>
-              </a>
+                Top
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                to="/"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                <Link to="/">Upvoted</Link>
-              </a>
+                Upvoted
+              </Link>
             </li>
-            <button className="mx-2 px-4 py-1 my-1 border border-blue-900 bg-blue-700 text-white item-center rounded-3xl hover:bg-blue-500">
+            <li>
+              <Link
+                to="/about"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                About
+              </Link>
+            </li>
+            <Link
+              to="/signup"
+              className="mx-2 px-4 py-1 my-1 border border-blue-900 bg-blue-700 text-white item-center rounded-3xl hover:bg-blue-500"
+            >
               Sign up
-            </button>
-            <button className="mx-2 space-x-2 my-1 space-y-2 px-4 py-1 border border-black bg-white text-black item-center rounded-3xl hover:bg-gray-200">
+            </Link>
+            <Link
+              to="/login"
+              className="mx-2 space-x-2 my-1 space-y-2 px-4 py-1 border border-black bg-white text-black item-center rounded-3xl hover:bg-gray-200"
+            >
               Log in
-            </button>
+            </Link>
           </ul>
         </div>
       </div>
---
+++ frontend/src/routes/signup.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+import { SignUp } from "@/components/auth/SignUp";
+
+export const Route = createFileRoute("/signup")({
+  component: SignUp,
+});
